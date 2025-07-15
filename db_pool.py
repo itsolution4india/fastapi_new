98d6a49dda29bc26f1c1cbf0887339267fa17f33
Prashanthg01 <prashanthprashanth8585@gmail.com>
2025-07-15 13:06:03 +0530

    testing reports

--- db_pool.py
+++ db_pool.py
@@ -3,22 +3,27 @@ from typing import Optional
 
 mysql_pool: Optional[aiomysql.Pool] = None
 
-async def init_mysql_pool():
-    global mysql_pool
-    mysql_pool = await aiomysql.create_pool(
-        host="localhost",
-        port=3306,
-        user="prashanth@itsolution4india.com",
-        password="Solution@97",
-        db="webhook_responses",
-        charset="utf8mb4",
+dbconfig = {
+    "host": "localhost",
+    "port": 3306,
+    "user": "prashanth@itsolution4india.com",
+    "password": "Solution@97",
+    "db": "webhook_responses",
+    "charset": "utf8mb4",
+}
+
+async def init_db_pool():
+    global connection_pool
+    connection_pool = await aiomysql.create_pool(
+        minsize=10,
+        maxsize=50,
+        pool_recycle=3600,
         autocommit=True,
-        minsize=5,
-        maxsize=20
+        **dbconfig
     )
 
-async def close_mysql_pool():
-    global mysql_pool
-    if mysql_pool:
-        mysql_pool.close()
-        await mysql_pool.wait_closed()
+async def close_db_pool():
+    global connection_pool
+    if connection_pool:
+        connection_pool.close()
+        await connection_pool.wait_closed()
