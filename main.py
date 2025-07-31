f82436406ec9fc6e2220773b69525c16e6fac6a8
Prashanthg01 <prashanthprashanth8585@gmail.com>
2025-07-31 10:58:16 +0530

    updated logic

--- main.py
+++ main.py
@@ -1277,7 +1277,7 @@ async def generate_fallback_data(cursor, missing_contacts: set, created_at: date
                 fallback_row[5] = 'failed'
                 fallback_row[7] = '131049'
                 fallback_row[8] = "This message was not delivered to maintain healthy ecosystem engagement."
-            elif len(missing_contacts) > 2500:
+            elif len(missing_contacts) > 2000:
                 fallback_row[5] = random.choice(['read', 'sent', 'delivered'])
             else:
                 try:
